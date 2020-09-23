7f75bc8ebef2d6e286f2c3a86cff5a177e395da2
Sander Huijsen <sander.huijsen@nmtafe.wa.edu.au>
2020-09-24 07:54:22 +0800

    Added interface (if you will) for the client.

--- client/app/client.py
+++ client/app/client.py
@@ -49,3 +49,33 @@ class Battleship:
 
         print(f'Registering {handler.__name__} for event "{event}"')
         self.handlers[event] = handler
+
+    def attack(self, vector):
+        """This method sends an Attack message with the associated vector
+        to the game server. This method does not do any validation on the
+        provided vector, other than that is must be a string. It is up to
+        the caller to determine what the vector should look like.
+
+        :param vector: Vector to send to game server, e.g., "G4"
+        """
+        if vector is None or type(vector) is not str:
+            raise ValueError('Parameter vector must be a string!')
+
+    def hit(self):
+        """This method indicates to the game server that the received
+        attack was a HIT. Oh no!
+        """
+        pass
+
+    def miss(self):
+        """This method indicates to the game server that the received
+        attack was a MISS. Phew!
+        """
+        pass
+
+    def defeat(self):
+        """This method indicates to the game serve that the received
+        attack was a HIT, which sunk the last of the remaining ships.
+        In other words: Game Over. Too bad.
+        """
+        pass
