49a0a10e5b282d2d8f07ffb4212acb02019b145a
Sander Huijsen <sander.huijsen@nmtafe.wa.edu.au>
2020-09-25 16:28:22 +0800

    First working version of reference implementation.

--- client/app/main.py
+++ client/app/main.py
@@ -11,13 +11,54 @@ def begin():
 
 @battleship.on()
 def start_turn():
-    print('Start turn received!')
+    s = input('Your move> ')
+    battleship.attack(s)
 
 
 @battleship.on()
-def end_turn():
-    print('End turn received!')
+def hit():
+    print('You hit the target!')
 
 
+@battleship.on()
+def miss():
+    print('Aww.. You missed!')
+
+
+@battleship.on()
+def attack(vector):
+    vector = vector[0]
+    print(f'Shot received at {vector}')
+    while True:
+        print("""H)it, m)iss, or d)efeat?""")
+        s = input('Enter status> ')
+        _s = s[0].upper()
+        if _s == 'H':
+            battleship.hit()
+            break
+        elif _s == 'M':
+            battleship.miss()
+            break
+        elif _s == 'D':
+            battleship.defeat()
+            break
+        else:
+            continue
+
+
+@battleship.on()
+def win():
+    print('Yay! You won!')
+    exit(0)
+
+
+@battleship.on()
+def lose():
+    print('Aww... You lost...')
+    exit(0)
+
+
+print('Waiting for the game to start.')
 battleship.join()
-time.sleep(120)
+while True:
+    time.sleep(1.0)
