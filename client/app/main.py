8d3023cdc519a3273d0c86c91b88ba0e1d92434f
Sander Huijsen <sander.huijsen@nmtafe.wa.edu.au>
2020-09-25 13:56:18 +0800

    Changed main to use the .on() decorator to register callback functions.

--- client/app/main.py
+++ client/app/main.py
@@ -1,6 +1,23 @@
-def main():
-    pass
+import time
+from client import Battleship
+
+battleship = Battleship()
 
 
-if __name__ == '__main__':
-    main()
+@battleship.on()
+def begin():
+    print('Game started!')
+
+
+@battleship.on()
+def start_turn():
+    print('Start turn received!')
+
+
+@battleship.on()
+def end_turn():
+    print('End turn received!')
+
+
+battleship.join()
+time.sleep(120)
