ea7142fe966f0bfea086f9961299c32789c3bff2
Sander Huijsen <sander.huijsen@gmail.com>
2021-06-27 15:59:52 +0800

    Added the option to show own/opponent's grid vertically or horizontally. (#23)
    * Added option to choose between horizontally and vertically placed fields.
    
    * Simplified how callback functions are handled.
    
    * Show coordinates in grid.
    
    * Fixed issue with order of UI grids. Show ship on UI grid as first letter of its name.
    
    * Renamed the start_turn and end_turn methods. Made AI slightly smarter by finding next attack closer to a previous hit.
    
    * Refactoring (code style).
    
    * Refactored some of the code that does nearby attacks. Added comments.

--- clients/random_ai/app/main.py
+++ clients/random_ai/app/main.py
@@ -18,19 +18,25 @@ grpc_port = os.getenv('GRPC_PORT', '50051')
 
 
 class Game(EasyFrame):
-    SHIPS = {'A': 5, 'B': 4, 'S': 3, 's': 3, '5': 3,
-             'C': 3, 'D': 2, 'd': 2, 'P': 1, 'p': 1,
-             }
+    SIZE = 10
+    SHIPS = {
+        'A': 5, 'B': 4, 'S': 3, 's': 3, '5': 3,
+        'C': 3, 'D': 2, 'd': 2, 'P': 1, 'p': 1,
+    }
 
-    SHIP_NAMES = {'A': 'Aircraft Carrier', 'B': 'Battleship',
-                  'S': 'Submarine', 's': 'Submarine', '5': 'Submarine',
-                  'C': 'Cruiser',
-                  'D': 'Destroyer', 'd': 'Destroyer',
-                  'P': 'Patrol Boat', 'p': 'Patrol Boat',
-                  }
+    SHIP_NAMES = {
+        'A': 'Aircraft Carrier',
+        'B': 'Battleship',
+        'S': 'Submarine', 's': 'Submarine', '5': 'Submarine',
+        'C': 'Cruiser',
+        'D': 'Destroyer', 'd': 'Destroyer',
+        'P': 'Patrol Boat', 'p': 'Patrol Boat',
+    }
 
-    def __init__(self, timeout=1.0, mirrored=False):
+    def __init__(self, timeout=1.0, mirrored=False, vertical=False, smart_ai=False):
         EasyFrame.__init__(self, 'Battleships')
+
+        self.__smart_ai = smart_ai
 
         # Get a copy of the ships
         self.__ships = self.SHIPS.copy()
@@ -38,18 +44,24 @@ class Game(EasyFrame):
         self.__mine = Battlefield(colour=193)
         self.__opponent = Battlefield(colour=208)
 
-        self.__mine_ui = BattlefieldUI(self, width=400, height=400, size=10)
-        self.__opponent_ui = BattlefieldUI(self, width=400, height=400, size=10,
+        self.__mine_ui = BattlefieldUI(self, width=400, height=400, size=self.SIZE)
+        self.__opponent_ui = BattlefieldUI(self, width=400, height=400, size=self.SIZE,
                                            colour='lightgreen')
 
+        fields = []
         if not mirrored:
-            # Top fields is opponent's
-            self.addCanvas(self.__opponent_ui, row=0)
-            self.addCanvas(self.__mine_ui, row=1)
+            fields.append(self.__opponent_ui)
+            fields.append(self.__mine_ui)
         else:
-            # Top field is mine
-            self.addCanvas(self.__mine_ui, row=0)
-            self.addCanvas(self.__opponent_ui, row=1)
+            fields.append(self.__mine_ui)
+            fields.append(self.__opponent_ui)
+
+        for i in range(len(fields)):
+            field = fields[i]
+            if vertical:
+                self.addCanvas(field, row=i, column=0)
+            else:
+                self.addCanvas(field, row=0, column=i)
 
         self.__timeout = abs(timeout)
         self.__attack_vector = None, None
@@ -65,8 +77,8 @@ class Game(EasyFrame):
 
         # Assign event handlers
         client.add_event_listener('begin', self.begin)
-        client.add_event_listener('start_turn', self.start_turn)
-        client.add_event_listener('end_turn', self.end_turn)
+        client.add_event_listener('start_turn', self.start_my_turn)
+        client.add_event_listener('end_turn', self.end_my_turn)
         client.add_event_listener('hit', self.hit)
         client.add_event_listener('miss', self.miss)
         client.add_event_listener('win', self.won)
@@ -97,7 +109,8 @@ class Game(EasyFrame):
                 result = self.__mine.place_ship(ship, x, y, size, horizontal=o)
                 if result is not None:
                     for x, y in result:
-                        self.__mine_ui.update_at(x, y, ship)
+                        ship_name = self.SHIP_NAMES[ship][0]
+                        self.__mine_ui.update_at(x, y, ship_name)
                     break
 
     def start(self):
@@ -106,34 +119,77 @@ class Game(EasyFrame):
     def begin(self):
         logger.info("The game has started!")
 
-    def start_turn(self):
+    def start_my_turn(self):
         logger.info("Okay, it's my turn now.")
         time.sleep(self.__timeout)
         while True:
-            col = random.randint(0, 9)
-            row = random.randint(0, 9)
-            cell = self.__opponent.get_by_col_row(col, row)
-            if cell is None:
-                self.__attack_vector = col, row
-                x, y = self.__mine.to_coords(col, row)
-                vector = f'{x}{y}'
-                logger.info(f'Attacking on {vector}.')
-                self.__client.attack(vector)
-                break
+            try:
+                self.__attack_nearby_cell()
+                return
+            except ValueError:
+                col = random.randint(0, 9)
+                row = random.randint(0, 9)
 
-    def end_turn(self):
+                cell = self.__opponent.get_by_col_row(col, row)
+                if cell is None:
+                    self.__attack_cell(col, row)
+                    return
+
+    def __attack_cell(self, col, row):
+        """
+        Attack the opponent at location ({col}, {row}).
+        """
+        self.__attack_vector = col, row
+        x, y = self.__mine.to_coords(col, row)
+        vector = f'{x}{y}'
+        logger.info(f'Attacking on {vector}.')
+        self.__client.attack(vector)
+
+    def __attack_nearby_cell(self):
+        """
+        This method attacks a nearby cell based on an earlier hit. The
+        position of the hit is maintained in the instance variable
+        {__attack_vector}. The AI will try to find an empty location
+        close to the latest successful attack. If no empty locations
+        exist, it will raise a ValueError.
+        """
+        col, row = self.__attack_vector
+        if self.__smart_ai and not (col is None or row is None):
+            relative_cells = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+
+            # Shuffling may not make much of a difference...
+            random.shuffle(relative_cells)
+            for dx, dy in relative_cells:
+                dcol, drow = col + dx, row + dy
+                if not (0 <= dcol < self.SIZE and 0 <= drow < self.SIZE):
+                    # Off the grid, so move to the next one
+                    continue
+
+                cell = self.__opponent.get_by_col_row(dcol, drow)
+                if cell is None:
+                    logger.info(f'Nearby empty cell @ ({col},{row}) found.')
+                    self.__attack_cell(dcol, drow)
+                    return
+
+        raise ValueError
+
+    def end_my_turn(self):
         logger.info("Okay, my turn has ended.")
 
     def hit(self):
         logger.info("Success!")
         self.__opponent.set_by_col_row(*self.__attack_vector, 'X')
         self.__opponent_ui.update_at(*self.__attack_vector, '\u2716', colour='red')
+        # Don't reset attack vector so AI will try to attack a nearby position
 
     def miss(self):
         logger.info("No luck.")
         dot = '\u25CB'
         self.__opponent.set_by_col_row(*self.__attack_vector, dot)
-        self.__opponent_ui.update_at(*self.__attack_vector, dot, colour='grey')
+        self.__opponent_ui.update_at(*self.__attack_vector, dot, colour='blue')
+
+        # Reset the attack vector so next attack will be at random position
+        self.__attack_vector = None, None
 
     def won(self):
         logger.info("I won!!!")
@@ -172,8 +228,10 @@ class Game(EasyFrame):
 
 def main():
     mirrored = os.getenv('MIRRORED', False)
+    vertical = os.getenv('VERTICAL', False)
+    smart_ai = os.getenv('SMART_AI', False)
 
-    game = Game(timeout=0.0, mirrored=mirrored)
+    game = Game(timeout=0.25, mirrored=mirrored, vertical=vertical, smart_ai=smart_ai)
     game.setup()
     game.start()
 
